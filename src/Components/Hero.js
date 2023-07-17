fcd61424b918b3fa2d090d3923267bc6cf953cff
Nahla31 <nahlagamal3125@gmail.com>
2023-07-18 00:31:38 +0300

    airbnb-clone

---
+++ src/Components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero--section">
+      <img src="./images/photo-grid.png" className="hero--img"></img>
+      <h1 className="hero--title">Online Experiences</h1>
+      <p className="hero--content">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
